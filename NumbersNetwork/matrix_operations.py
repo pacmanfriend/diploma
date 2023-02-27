cb0a6b550812913b88d4b91df0d697efd620b995
Vladislav Antonov <lolik1806@gmail.com>
2023-02-27 09:26:36 +0300

    fix

--- NumbersNetwork/matrix_operations.py
+++ NumbersNetwork/matrix_operations.py
@@ -1,3 +1,6 @@
+import random
+
+
 def weight_vect_sum(vect_a, vect_b) -> float:
     assert (len(vect_a) == len(vect_b))
 
@@ -27,6 +30,16 @@ def scalar_mult(scalar, vect) -> list:
     return vect
 
 
+def matrix_sub(matrix_a, matrix_b) -> list[list[float]]:
+    assert (len(matrix_a) == len(matrix_b))
+
+    for i in range(len(matrix_a)):
+        for j in range(len(matrix_b)):
+            matrix_a[i][j] -= matrix_b[i][j]
+
+    return matrix_a
+
+
 def calc_weight_deltas(vect_a, vect_b) -> list[list]:
     result = [[0.0 for _ in range(len(vect_b))] for _ in range(len(vect_a))]
 
@@ -35,3 +48,13 @@ def calc_weight_deltas(vect_a, vect_b) -> list[list]:
             result[i][j] = vect_a[i] * vect_b[j]
 
     return result
+
+
+def generate_random_matrix(n, m) -> list[list[float]]:
+    matrix = [[0.0 for _ in range(m)] for _ in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            matrix[i][j] = random.random()
+
+    return matrix
